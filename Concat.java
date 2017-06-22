33212883922516b518e76885b64aa389c381823c
sushmaallamraju <sushma.allamraju@gmail.com>
2017-06-22 15:35:05 -0700

    My recent comment2

---
+++ Concat.java
@@ -0,0 +1,12 @@
+public class Concat{
+public static void main(String [] args){
+
+String s1 = "Sushma";
+String s2 = "Kumar";
+String s3 = s1 + s2;
+System.out.println(s3);
+System.out.println(s3);
+
+
+}
+}
