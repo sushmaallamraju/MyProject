892d5725027acaf2c5a99e1e6cc5e9144226a4a5
sushmaallamraju <sushma.allamraju@gmail.com>
2017-06-30 12:01:39 -0700

    This is adding TestClass.java

---
+++ TestClass.java
@@ -0,0 +1,15 @@
+public class TestClass {
+
+	
+  static {
+    System.out.println("Hello, World!");
+  }
+
+
+    public static void main(String[] args) {
+    
+      //TestClass t = new TestClass();
+      //t.sushma();
+    }
+
+}
