a59ba7fe1a9d9ae874b1564516fbf611faa6339a
“sushmaallamraju” <sushma.allamraju@gmail.com>
2017-11-19 17:07:23 -0800

    com

--- Test_ngTest.java
+++ Test_ngTest.java
@@ -1,39 +1,41 @@
 package MTestProj.MavenTestProj;
 
+import java.util.concurrent.TimeUnit;
+
 import org.openqa.selenium.By;			
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;			
-import org.testng.Assert;			
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.testng.Assert;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;			
 
 public class Test_ngTest {
-		
-	    WebDriver driver;			
-
-	    // Method 1: Open Browser say Firefox			
-	    @Test (priority=1)		
-	    public void openBrowser() {				
-	    	System.setProperty("webdriver.chrome.driver","/Users/sushmaallamraju/software/sel1/selenium-2.53.1/chromedriver");
-	         driver = new ChromeDriver();   			
-	    }		
-
-	    // Method 2: Launch Google.com			
-	    @Test (priority=2)		
-	    public void launchGoogle() {				
-	        driver.get("http://www.google.co.in");						
-	    }		
-
-	    // Method 3: Perform a search using "Facebook"			
-	    @Test (priority=3)		
-	    public void peformSeachAndClick1stLink() {				
-	        driver.findElement(By.xpath(".//*[@title='Search']")).sendKeys("Facebook");								
-	    }		
-
-	    // Method 4: Verify Google search page title.			
-	    @Test (priority=4)		
-	    public void FaceBookPageTitleVerification() throws Exception {				
-	        driver.findElement(By.name("btnG")).click();						
-	        Thread.sleep(3000);		
-	        Assert.assertEquals(driver.getTitle().contains("Facebook - Google Search"), true);				
-	    }		
+	 WebDriver driver;
+	    
+	    @BeforeTest
+	    public void setup() throws Exception {
+	    	 System.setProperty("webdriver.chrome.driver","/Users/sushmaallamraju/software/sel1/selenium-2.53.1/chromedriver"); 
+	 		driver = new ChromeDriver();
+	         driver.manage().window().maximize();
+	         driver.manage().timeouts().implicitlyWait(15, TimeUnit.SECONDS);
+	         driver.get("https://www.facebook.com/"); 
+	    } 
+	    
+	   @Test
+	   public void loginMethod() throws InterruptedException{
+		 driver.findElement (By.id("email")).sendKeys("sushma.allamraju@gmail.com");
+		  //driver.findElement(By.xpath(".//*[@id='identifierNext']/content/span")) .click();
+		  driver.findElement(By.id("pass")).sendKeys("Harish09");
+		  Thread.sleep(2000);
+		  driver.findElement(By.id("loginbutton")).submit();
+	   }
+	   
+	    @AfterTest
+	         public void tearDown() throws Exception { 
+	    	Thread.sleep(3000);
+	         driver.quit(); 
+	    } 
 	}	
